5df49563ca5a83d2987783599b389e3677a92793
PaulParizacaMozo <pparizaca@unsa.edu.pe>
2023-09-12 20:26:57 -0500

    Se creo el archivo main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "headers/circulo.h"
+#include "headers/figura.h"
+#include "headers/triangulo.h"
+#include "headers/cuadrado.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<"Inicio del proyecto\n";
+    return 0;
+}
